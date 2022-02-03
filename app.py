5c46b4392d259dc681e01fc97f61020b06d95898
Ehsan <ehsanghazanfar2@gmail.com>
2022-02-03 23:21:52 +0500

    Subscriber is now working

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
+import re
 from flask import Flask, render_template, Response,request
 import cv2
 import base64
 import time
 import os
+from twilio.rest import Client
+import requests as req
+from flask import jsonify
+
+config_firebase ={
+    "apiKey": "AIzaSyDatGf-08IgqLhQX7fliKu4Mjyh5VPuHjc",
+  "authDomain": "trie-994c1.firebaseapp.com",
+  "projectId": "trie-994c1",
+  "storageBucket": "trie-994c1.appspot.com",
+  "messagingSenderId": "815090125887",
+  "appId": "1:815090125887:web:acd4a2a3346b7ace447c8a",
+  "serviceAccount":"serciveaccountkey.json"
+}
+
+
 cur_path = os.path.dirname(__file__)
 app=Flask(__name__)
 camera=cv2.VideoCapture(0)
@@ -33,12 +49,65 @@ def generate_frames():
 
 @app.route("/")
 def index():
+    send_notifications_wrapper()
     return render_template("index.html")
+
+
+@app.route("/call_sim", methods=['POST'])
+def call_sim():
+    account_sid='ACa70d045e9ea85caa79c86ebfb3e5300a'
+    auth_token='c37a229fba02be84ce3d96292e14a309'
+    request_data = request.get_json()
+    number= request_data["PhoneNumber"]
+    print(number)
+    client=Client(account_sid,auth_token)
+    call=client.calls.create(twiml='<Resposne><Say>hello success learner</Say></Response>',to=number,from_='+16205778296')
+    print(call.sid)
+    if call.sid:
+        return (Response("Done",200))
+    else:
+        return (Response("No",404))
 
 
 @app.route("/video")
 def video():
+    
     return Response(generate_frames(),mimetype='multipart/x-mixed-replace; boundary=frame' )
+
+
+def check_subscribers(token):
+    try:
+        file= open("Subscribers.txt","r")
+    
+        readfile = file.read()
+        if token in readfile: 
+            return True
+        else:
+            return False
+    except:
+        return False
+    
+
+@app.route("/subscribe", methods=['POST'])
+def subscribe():
+    token= request.get_json()
+    print(token)
+    token=token["to"]
+    print(type(token))
+    flag=check_subscribers(token)
+    if flag != True:
+        f = open("Subscribers.txt", "a")
+        f.write(token)
+        f.write("\n")
+        f.close()
+        x={"result":"OK"}
+        return jsonify(x)
+    else:
+        x={"result":"Already Subscribed"}
+        return jsonify(x)
+    
+
+    
 
 @app.route("/ehsan")
 def ehsan():
@@ -68,6 +137,28 @@ def post_images():
         fh.write(base64.decodebytes(obj))
     
     return (Response(),200)
+
+def send_notifications_wrapper():
+    try:
+        file= open("Subscribers.txt","r")
+    
+        readfile = file.read().splitlines()
+        count=0
+        for line in readfile:
+           
+            send_notifications(line,"Video Made","Press to view")
+    except:
+        return False
+
+def send_notifications(expo_token, title, body):
+  print("Inside")  
+  message = {
+    'to' : expo_token,
+    'title' : title,
+    'body' : body
+  }
+  return req.post('https://exp.host/--/api/v2/push/send', json = message)
+
        
 if __name__ == "__main__":
    app.run(host="0.0.0.0")
