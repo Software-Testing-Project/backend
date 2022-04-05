16c49d5236eccce6f5e5591500ebbe1bed64fe13
Ehsan <l181092@lhr.nu.edu.pk>
2022-04-06 00:22:40 +0500

    Working NLP

--- app.py
+++ app.py
@@ -18,6 +18,157 @@ from writeThreadDateTimeName import writeThreadDateTimeName
 from werkzeug.utils import secure_filename
 from werkzeug.datastructures import FileStorage
 
+import speech_recognition as sr
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import wordnet
+from nltk.corpus import wordnet as wn
+import datetime
+from pydub import AudioSegment
+from os import path
+
+
+################## NLP###########################
+def convert_file():
+    m4a_file = 'test.m4a'
+    wav_filename = r"abc.wav"
+    track = AudioSegment.from_file(m4a_file,  format='m4a')
+    file_handle = track.export(wav_filename, format='wav')
+
+
+# nltk.download('wordnet')
+MONTHS = ["january", "february", "march", "april", "may", "june",
+          "july", "august", "september", "october", "november", "december"]
+DAYS = ["monday", "tuesday", "wednesday",
+        "thursday", "friday", "saturday", "sunday"]
+DAY_EXTENTIONS = ["rd", "th", "st", "nd"]
+NAME = ['ahmed', 'hassan']
+START_DATE = datetime.date(2021, 2, 1)
+
+
+def fetch_audio_text():
+    text = None
+    r = sr.Recognizer()
+    # with sr.Microphone() as source:
+    AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "abc.wav")
+    with sr.AudioFile(AUDIO_FILE) as source:
+        r.adjust_for_ambient_noise(source)
+        print("Talk")
+        # <class 'speech_recognition.AudioData'>
+        #audio_text = r.record(source, duration=20)
+        audio_text = r.record(source)  # read the entire audio file
+        print("Time over, thanks")
+        try:
+            text = r.recognize_google(audio_text)
+            print("you said: " + text)
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print(
+                "Could not request results from Google Speech Recognition service; {0}".format(e))
+    return text
+
+
+def fetch_synonyms(words):
+    synonyms = []
+    for word in words:
+        for syn in wn.synsets(word):
+            for l in syn.lemmas():
+                synonyms.append(l.name())
+    synonym_verbs = []
+    lemmatizer = WordNetLemmatizer()
+    for word in synonyms:
+        synonym_verbs.append(lemmatizer.lemmatize(word, pos='v'))
+    synonym_verbs = list(set(synonym_verbs))
+    return synonym_verbs
+
+
+def process_text(audio_text):
+    word_list = nltk.word_tokenize(audio_text)
+    lemmatizer = WordNetLemmatizer()
+    lemmatized_list = []
+    for word in word_list:
+        lemmatized_list.append(lemmatizer.lemmatize(word, pos='v'))
+    return lemmatized_list
+
+
+def search_action(processed_text, synonyms):
+    for word in processed_text:
+        for key_action in synonyms:
+            if word in synonyms[key_action]:
+                return key_action
+
+
+def search_name(processed_text):
+    names = []
+    for word in processed_text:
+        if word in NAME:
+            names.append(word)
+    return names
+
+
+def is_date_feasible(day, month, year):
+    isValidDate = True
+    try:
+        date = datetime.date(year, month, day)
+    except ValueError:
+        isValidDate = False
+    if isValidDate:
+        if date >= START_DATE and date <= datetime.date.today():
+            return True
+    return False
+
+
+def search_date(processed_text):
+    day = month = year = None
+    for i in range(len(processed_text)):
+        processed_text[i] = processed_text[i]. lower()
+        if processed_text[i] in MONTHS:
+            month = MONTHS.index(processed_text[i])+1
+        if processed_text[i][-2:] in DAY_EXTENTIONS and processed_text[i][:-2].isdigit():
+            day = int(processed_text[i][:-2])
+        if processed_text[i][:4].isdigit():
+            year = int(processed_text[i][:4])
+            break
+    if day != None and month != None and year != None:
+        if is_date_feasible(day, month, year):
+            return datetime.date(year, month, day)
+    else:
+        return None
+
+
+def is_time_feasible(hours, minutes):
+    isValidTime = True
+    try:
+        time = datetime.time(hours, minutes, 0)
+    except ValueError:
+        isValidTime = False
+    return isValidTime
+
+
+def search_time(processed_text):
+    hour = minutes = None
+    if 'p.m' or 'a.m' in processed_text:
+        for item in processed_text:
+            if ':' in item:
+                if item[:2].isdigit() and item[3:].isdigit():
+                    if 'a.m' in processed_text:
+                        hour = int(item[:2])
+                        minutes = int(item[3:])
+                        break
+                    elif 'p.m' in processed_text:
+                        hour = int(item[:2])+12
+                        minutes = int(item[3:])
+                        break
+    if hour != None and minutes != None:
+        if is_time_feasible(hour, minutes):
+            return datetime.time(hour, minutes, 0)
+    else:
+        return None
+
+#####################################
+
+
 counter = Value('i', 0)
 
 config_firebase = {
@@ -141,30 +292,10 @@ def ehsan():
 @app.route("/Postimages",  methods=['POST'])
 def post_images():
     r = request
-    obj = r.get_json()
-    obj = obj["img"]
-    obj = base64.b64decode(obj)
-    print(type(obj))
-
-    # convert string of image data to uint8
-    # decode image
-   # img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
-    name = "Ahamd"
-    t = time.localtime()
-
-    img_name = name+"_" + str(t.tm_mday)+"_"+str(t.tm_mon)+"_"+str(
-        t.tm_year)+"_"+str(t.tm_hour)+"_"+str(t.tm_min)+"_"+str(t.tm_sec)
-    img_name += ".jpg"
-
-    save_path = "./"+name
-    completeName = os.path.join(save_path, img_name)
-    checkpath(save_path)
-
-    with open(completeName, "wb") as fh:
-        fh.write(obj)
-
-    return (Response(), 200)
+    f = request.files['file']
+    f.save(secure_filename(f.filename))
+    x = {"message": "Yes"}
+    return jsonify(x)
 
 
 def send_notifications_wrapper():
@@ -194,7 +325,18 @@ def send_notifications(expo_token, title, body):
 def voice():
     f = request.files['file']
     f.save(secure_filename(f.filename))
-    return (Response(), 200)
+    convert_file()
+    synonyms = {}
+    synonyms['search'] = fetch_synonyms(['search', 'find'])
+    synonyms['set'] = fetch_synonyms(['set'])
+    audio_text = fetch_audio_text()
+    processed_text = process_text(audio_text)
+    action_verb = search_action(processed_text, synonyms)
+    date = search_date(processed_text)
+    names = search_name(processed_text)
+    time = search_time(processed_text)
+    x = {"message": audio_text}
+    return jsonify(x)
 
 
 if __name__ == "__main__":
